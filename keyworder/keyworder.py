57a083bb679389b3133c674f8998a2d0d695fca9
Tony Tang <tonytang215@hotmail.com>
2018-11-20 02:10:26 +1100

    Added functionality to Keyworder

--- keyworder/keyworder.py
+++ keyworder/keyworder.py
@@ -10,28 +10,46 @@ class Keyworder:
     """
     def __init__(self, url="https://stackoverflow.com"):
         self.url = url
-        self.page = requests.get(url)
+        self.page = requests.get(self.url)
         self.visible_text = visible_text_from_html(self.page.content)
 
-        # Build a collection of standalone words through preprocessing the visible_text
         self.keyword_collection = []
+        self.keyword_count = {}
+        self.__count_keywords()
+    
+    def print_keywords(self, lines=10):
+        # Output the top keywords
+        print("Top keywords from " + str(self.url))
+        print_count = 1
+        total_keywords = 0
+        print("%4s %5s %15s" % ("Rank", "Freq", "Keyword"))
+        for keyword in sorted(self.keyword_count, key=self.keyword_count.get, reverse=True):
+            print("%4d %5d %15s" % (print_count, self.keyword_count[keyword], keyword))
+            print_count += 1
+            total_keywords += self.keyword_count[keyword]
+            if (print_count > lines):
+                break
+        print ("%4d total keywords, %4d unique keywords" % (total_keywords, len(self.keyword_count)))
+    
+    def set_url(self, new_url):
+        self.url = new_url
+        self.refresh()
+    
+    def refresh(self):
+        self.page = requests.get(self.url)
+        self.visible_text = visible_text_from_html(self.page.content)
+        
+        self.keyword_collection = []
+        self.keyword_count = {}
+        self.__count_keywords()
+    
+    def __count_keywords(self):
+        # Build a collection of standalone words through preprocessing the visible_text
         for sentence in self.visible_text:
             self.keyword_collection.extend(filter_string_for_keywords(sentence))
-
         # Count the occurence of each keyword and store it into a dict
-        self.keyword_count = {}
         for keyword in self.keyword_collection:
             if not keyword in self.keyword_count:
                 self.keyword_count[keyword] = 1
             else:
                 self.keyword_count[keyword] += 1
-    
-    def print_keywords(self, num=10):
-        # Output the top keywords
-        print(str(len(self.keyword_count)) + " visible unique keywords in " + str(self.url))
-        print_count = 0
-        for keyword in sorted(self.keyword_count, key=self.keyword_count.get, reverse=True):
-            print(print_count, self.keyword_count[keyword], keyword)
-            print_count += 1
-            if (print_count >= num):
-                break
