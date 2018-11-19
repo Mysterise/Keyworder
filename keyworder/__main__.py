e72dd9f5d5295b64c396a8a2580bba3917301f7f
Tony Tang <tonytang215@hotmail.com>
2018-11-20 00:54:51 +1100

    hardcoding cli args

--- keyworder/__main__.py
+++ keyworder/__main__.py
@@ -4,8 +4,13 @@ import sys
 from .keyworder import Keyworder
 
 def main():
-	URL = sys.argv[1] if (len(sys.argv) > 1) else DEFAULT_URL
+	URL = None
 	words_to_print = 10
+
+	if (len(sys.argv) > 1):
+		 URL = sys.argv[1] 
+	if (len(sys.argv) > 2):
+		words_to_print = sys.argv[2]
 
 	kw = Keyworder(URL)
 	kw.print_keywords(words_to_print)
